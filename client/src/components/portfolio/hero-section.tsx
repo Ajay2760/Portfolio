a27d946b590dd6e1d6d03a57aff7822761fc0c66
Ajay2760 <zeusgamer0157@gmail.com>
2025-10-07 11:35:28 +0530

    Change Resume updated

--- client/src/components/portfolio/hero-section.tsx
+++ client/src/components/portfolio/hero-section.tsx
@@ -29,7 +29,7 @@ const HeroSection = () => {
   const handleDownloadResume = () => {
     const link = document.createElement("a");
     link.href =
-      "https://drive.google.com/file/d/1bibCMwunH2MuJsnLsu6CZPKFK5gzVd2P/view?usp=sharing";
+      "https://drive.google.com/file/d/1j1KJxvJ2UWPDPE26wnUzd7xTzMqrQ9Av/view?usp=sharing";
     link.target = "_blank";
     link.rel = "noopener noreferrer";
     link.click();
